64e06e7bd6c7e92593dd97385b0cab7d82c8a3ea
Kartheek Palepu <kartheekpnsn@gmail.com>
2020-01-31 10:27:49 +0530

    Create fetch_imports.py

---
+++ Python/fetch_imports.py
@@ -0,0 +1,34 @@
+import os
+from fnmatch import fnmatch
+
+root = "PROJECT_PATH/src"
+
+pattern = "*.py"
+
+def get_files(files, pattern = "*.py"):
+	return [os.path.join(path, name) for name in files if fnmatch(name, pattern)]
+
+python_files = []
+for path, subdirs, files in os.walk(root):
+    python_files.extend(get_files(files))
+
+def is_import(line):
+	flag = False
+	if 'from ' in line:
+		if 'import ' in line:
+			flag = True
+	elif 'import ' in line:
+		flag = True
+	return flag
+
+def get_imports(file):
+	with open(file, 'r') as fObj:
+		lines = fObj.read().splitlines()
+	return [line.strip() for line in lines if is_import(line)]
+
+all_imports = []
+for file in python_files:
+	all_imports.extend(get_imports(file))
+
+with open("all_imports_pcb.txt", "w") as fObj:
+	fObj.write("\n".join(list(set(all_imports))))
